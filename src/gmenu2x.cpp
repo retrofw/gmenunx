4e728c1904b9a0e5cfb353d448813b071e44ac00
pingflood <pingflood@gmail.com>
2018-05-01 21:55:07 -0300

    Comment out unneeded variable

--- src/gmenu2x.cpp
+++ src/gmenu2x.cpp
@@ -2063,7 +2063,6 @@ void GMenu2X::editLink() {
 	stringstream ss;
 	ss << DEFAULT_CPU_CLK;
 	ss >> strClock;
-	const string wd = "/"; //menu->selLinkApp()->getRealWorkdir();
 
 	SettingsDialog sd(this, input, ts, diagTitle, diagIcon);
 	sd.addSetting(new MenuSettingString(      this, tr["Title"],                tr["Link title"], &linkTitle, diagTitle, diagIcon ));
