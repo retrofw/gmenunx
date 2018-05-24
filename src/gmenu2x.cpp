b07464a88c1555caaee2e1e193636ddadcdf85cc
pingflood <pingflood@gmail.com>
2018-05-23 21:45:55 -0300

    Set limits to minBattery and maxBattery

--- src/gmenu2x.cpp
+++ src/gmenu2x.cpp
@@ -738,6 +738,9 @@ void GMenu2X::readConfig() {
 	evalIntConf( &confInt["videoBpp"], 16, 8, 32 );
 	evalIntConf( &confInt["backlight"], 70, 1, 100);
 
+	evalIntConf( &confInt["minBattery"], 0, 1, 10000);
+	evalIntConf( &confInt["maxBattery"], 4500, 1, 10000);
+
 	// evalIntConf( &confInt["sectionBar"], SB_LEFT, 1, 4);
 	confInt["sectionBar"] = SB_LEFT;
 
@@ -2206,6 +2209,8 @@ if (confStr["batteryType"] == "RS-97") {
 		}
 	}
 #else
+
+	val = constrain(val, 0, 4500);
 
 	bool needWriteConfig = false;
 	long max = confInt["maxBattery"];
