4482b99901f99a46f836e3fb5d455831ae9ab505
pingflood <pingflood@gmail.com>
2018-07-09 22:21:04 -0300

    Normalize button label

--- src/wallpaperdialog.cpp
+++ src/wallpaperdialog.cpp
@@ -67,7 +67,7 @@ bool WallpaperDialog::exec()
 		gmenu2x->s->box(gmenu2x->listRect, gmenu2x->skinConfColors[COLOR_LIST_BG]);
 
 		gmenu2x->drawButton(gmenu2x->s, "a", gmenu2x->tr["Select"],
-		gmenu2x->drawButton(gmenu2x->s, "b", gmenu2x->tr["Back"],5));
+		gmenu2x->drawButton(gmenu2x->s, "start", gmenu2x->tr["Exit"],5));
 
 		//Selection
 		if (selected >= firstElement + numRows) firstElement = selected - numRows;
