ef2afe192a9281fe750c7f2311ed7ec5fe5fc53e
pingflood <pingflood@gmail.com>
2018-07-12 17:53:52 -0300

    Add icon to messagebox

--- src/gmenu2x.cpp
+++ src/gmenu2x.cpp
@@ -1524,7 +1524,6 @@ void GMenu2X::explorer() {
 	}
 }
 
-
 void GMenu2X::ledOn() {
 #if defined(TARGET_GP2X)
 	if (memdev != 0 && !f200) memregs[0x106E >> 1] ^= 16;
@@ -1683,7 +1682,7 @@ void GMenu2X::umountSdDialog() {
 	mb.setButton(CANCEL,  tr["No"]);
 	if (mb.exec() == CONFIRM) {
 		umountSd();
-		MessageBox mb(this, tr["Complete!"]);
+		MessageBox mb(this, tr["Complete!"], "skin:icons/eject.png");
 		mb.exec();
 		// menu->deleteSelectedLink();
 	}
