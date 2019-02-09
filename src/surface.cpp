da6ba3f639d31dd8765e3656120d7821b09ba105
pingflood <pingflood@gmail.com>
2019-02-09 14:20:38 -0200

    Return black image if source image is corrupt

--- src/surface.cpp
+++ src/surface.cpp
@@ -150,6 +150,19 @@ void Surface::load(const string &img, bool alpha, const string &skin) {
 			enableAlpha();
 	} else {
 		ERROR("Couldn't load surface '%s'", img.c_str());
+		uint32_t rmask, gmask, bmask, amask;
+		#if SDL_BYTEORDER == SDL_BIG_ENDIAN
+			rmask = 0xff000000;
+			gmask = 0x00ff0000;
+			bmask = 0x0000ff00;
+			amask = 0x000000ff;
+		#else
+			rmask = 0x000000ff;
+			gmask = 0x0000ff00;
+			bmask = 0x00ff0000;
+			amask = 0xff000000;
+		#endif
+		raw = SDL_DisplayFormat( SDL_CreateRGBSurface(SDL_SWSURFACE|SDL_SRCALPHA, 16, 16, 16, rmask, gmask, bmask, amask ) );
 	}
 }
 
