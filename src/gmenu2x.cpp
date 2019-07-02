4b91db283d1f8891aeefd2bdee85bc85a6a113b7
pingflood <pingflood@gmail.com>
2019-07-02 10:59:20 -0300

    Show BG only if fits

--- src/gmenu2x.cpp
+++ src/gmenu2x.cpp
@@ -473,19 +473,17 @@ void GMenu2X::main() {
 					ix = linksRect.x + x * linkWidth  + (x + 1) * linkSpacing;
 					iy = linksRect.y + y * linkHeight + (y + 1) * linkSpacing;
 
-					if (i == (uint32_t)menu->selLinkIndex() && iconBGon == NULL)
-						s->box(ix + (linkWidth - min(linkWidth, icon->width())) / 2 - 4, iy + (linkHeight - min(linkHeight, icon->height())) / 2 - 4, min(linkWidth, icon->width()) + 8, min(linkHeight, icon->height()) + 8, skinConfColors[COLOR_SELECTION_BG]);
-					else if (iconBGoff != NULL)
+					if (i == (uint32_t)menu->selLinkIndex()) {
+						if (iconBGon != NULL && icon->width() <= iconBGon->width() && icon->height() <= iconBGon->height())
+							iconBGon->blit(s, {ix + iconPadding/2, iy + iconPadding/2, linkWidth - iconPadding, linkHeight - iconPadding}, HAlignCenter | VAlignMiddle, 50);
+						else
+							s->box(ix + (linkWidth - min(linkWidth, icon->width())) / 2 - 4, iy + (linkHeight - min(linkHeight, icon->height())) / 2 - 4, min(linkWidth, icon->width()) + 8, min(linkHeight, icon->height()) + 8, skinConfColors[COLOR_SELECTION_BG]);
+
+					} else if (iconBGoff != NULL)
 						iconBGoff->blit(s, {ix + iconPadding/2, iy + iconPadding/2, linkWidth - iconPadding, linkHeight - iconPadding}, HAlignCenter | VAlignMiddle);
 
 					icon->blit(s, {ix + iconPadding/2, iy + iconPadding/2, linkWidth - iconPadding, linkHeight - iconPadding}, HAlignCenter | VAlignMiddle);
 
-					// s->setClipRect({ix, iy, linkWidth, linkHeight});
-					if (i == (uint32_t)menu->selLinkIndex() && iconBGon != NULL)
-						iconBGon->blit(s, {ix + iconPadding/2, iy + iconPadding/2, linkWidth - iconPadding, linkHeight - iconPadding}, HAlignCenter | VAlignMiddle, 50);
-					// s->clearClipRect();
-
-					// if (skinConfInt["linkLabel"]) s->write(font, tr.translate(menu->sectionLinks()->at(i)->getTitle()), ix + 2 + linkWidth/2, iy + min(linkHeight, icon->height()) - 4 , HAlignCenter | VAlignBottom);
 					if (skinConfInt["linkLabel"]) s->write(font, tr.translate(menu->sectionLinks()->at(i)->getTitle()), ix + 2 + linkWidth/2, iy + (linkHeight + min(linkHeight, icon->height()))/2, HAlignCenter | VAlignMiddle);
 				}
 			}
