7b52ac22bb31828d2a00ea88ce445cb56166bcb6
pingflood <pingflood@gmail.com>
2020-01-24 10:11:12 -0300

    Initialize powerManager after SDL

--- src/gmenu2x.cpp
+++ src/gmenu2x.cpp
@@ -203,7 +203,6 @@ void GMenu2X::main() {
 	setVolume(confInt["globalVolume"]);
 
 	setSkin(confStr["skin"], true);
-	powerManager = new PowerManager(this, confInt["backlightTimeout"], confInt["powerTimeout"]);
 
 	setenv("SDL_FBCON_DONT_CLEAR", "1", 0);
 	setenv("SDL_NOMOUSE", "1", 1);
@@ -234,6 +233,8 @@ void GMenu2X::main() {
 	setBackground(s, currBackdrop);
 
 	bg = new Surface(s);
+
+	powerManager = new PowerManager(this, confInt["backlightTimeout"], confInt["powerTimeout"]);
 
 	MessageBox mb(this, tr["Loading"]);
 	mb.setAutoHide(1);
