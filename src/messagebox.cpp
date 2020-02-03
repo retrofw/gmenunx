b2ec4bccfd42e3f959d80e89786be9325457b203
pingflood <pingflood@gmail.com>
2020-02-03 12:10:13 -0300

    Animate screen fading

--- src/messagebox.cpp
+++ src/messagebox.cpp
@@ -24,12 +24,14 @@
 using namespace std;
 
 MessageBox::MessageBox(GMenu2X *gmenu2x, vector<MenuOption> options) {
-	Surface bg(gmenu2x->s);
 	bool close = false, inputAction = false;
 	int sel = 0;
 	uint32_t i, fadeAlpha = 0, h = gmenu2x->font->getHeight(), h2 = gmenu2x->font->getHalfHeight();
-
 	SDL_Rect box;
+
+	Surface bg(gmenu2x->s);
+	gmenu2x->powerManager->clearTimer();
+
 	box.h = h * options.size() + 8;
 	box.w = 0;
 	for (i = 0; i < options.size(); i++) {
@@ -48,8 +50,8 @@ MessageBox::MessageBox(GMenu2X *gmenu2x, vector<MenuOption> options) {
 		gmenu2x->s->box(box.x, box.y, box.w, box.h, gmenu2x->skinConfColors[COLOR_MESSAGE_BOX_BG]);
 		gmenu2x->s->rectangle(box.x + 2, box.y + 2, box.w - 4, box.h - 4, gmenu2x->skinConfColors[COLOR_MESSAGE_BOX_BORDER]);
 
-		//draw selection rect
-		gmenu2x->s->box( box.x + 4, box.y + 4 + h * sel, box.w - 8, h, gmenu2x->skinConfColors[COLOR_MESSAGE_BOX_SELECTION] );
+		// draw selection rect
+		gmenu2x->s->box(box.x + 4, box.y + 4 + h * selected, box.w - 8, h, gmenu2x->skinConfColors[COLOR_MESSAGE_BOX_SELECTION]);
 		for (i = 0; i < options.size(); i++)
 			gmenu2x->s->write(gmenu2x->font, options[i].text, box.x + 12, box.y + h2 + 3 + h * i, VAlignMiddle, gmenu2x->skinConfColors[COLOR_FONT_ALT], gmenu2x->skinConfColors[COLOR_FONT_ALT_OUTLINE]);
 
@@ -59,6 +61,7 @@ MessageBox::MessageBox(GMenu2X *gmenu2x, vector<MenuOption> options) {
 			fadeAlpha = intTransition(0, 200, tickStart, 200);
 			continue;
 		}
+
 		do {
 			inputAction = gmenu2x->input.update();
 
@@ -75,6 +78,8 @@ MessageBox::MessageBox(GMenu2X *gmenu2x, vector<MenuOption> options) {
 			}
 		} while (!inputAction);
 	}
+	gmenu2x->input.dropEvents(); // prevent passing input away
+	gmenu2x->powerManager->resetSuspendTimer();
 }
 
 MessageBox::MessageBox(GMenu2X *gmenu2x, const string &text, const string &icon) {
@@ -132,19 +137,17 @@ void MessageBox::setBgAlpha(bool bgalpha) {
 }
 
 int MessageBox::exec() {
-	int result = -1;
+	int result = -1, fadeAlpha = 0, ix = 0;
+	SDL_Rect box;
 
+	Surface bg(gmenu2x->s);
 	gmenu2x->powerManager->clearTimer();
 
-	//Darken background
-	gmenu2x->s->box((SDL_Rect){0, 0, gmenu2x->resX, gmenu2x->resY}, (RGBAColor){0,0,0,bgalpha});
-
-	SDL_Rect box;
 	box.h = gmenu2x->font->getTextHeight(text) * gmenu2x->font->getHeight() + gmenu2x->font->getHeight();
 	if (gmenu2x->sc[icon] != NULL && box.h < 40) box.h = 48;
 
-	box.w = gmenu2x->font->getTextWidth(text) + 24;// + (gmenu2x->sc[icon] != NULL ? 42 : 0);
-	int ix = 0;
+	box.w = gmenu2x->font->getTextWidth(text) + 24;
+
 	for (uint32_t i = 0; i < buttonText.size(); i++) {
 		if (!buttonText[i].empty())
 			ix += gmenu2x->font->getTextWidth(buttonText[i]) + 24;
@@ -154,53 +157,66 @@ int MessageBox::exec() {
 	if (ix > box.w) box.w = ix;
 
 	ix = (gmenu2x->sc[icon] != NULL ? 42 : 0);
-	box.w += ix;
 
+	box.w += ix;
 	box.x = (gmenu2x->resX - box.w) / 2 - 2;
 	box.y = (gmenu2x->resY - box.h) / 2 - 2;
 
-	//outer box
-	gmenu2x->s->box(box, gmenu2x->skinConfColors[COLOR_MESSAGE_BOX_BG]);
-	
-	//draw inner rectangle
-	gmenu2x->s->rectangle(box.x + 2, box.y + 2, box.w - 4, box.h - 4, gmenu2x->skinConfColors[COLOR_MESSAGE_BOX_BORDER]);
+	uint32_t tickStart = SDL_GetTicks();
+	do {
+		bg.blit(gmenu2x->s, 0, 0);
 
-	//icon+text
-	if (gmenu2x->sc[icon] != NULL) {
-		gmenu2x->s->setClipRect({box.x + 8, box.y + 8, 32, 32});
-		gmenu2x->sc[icon]->blit(gmenu2x->s, box.x + 24, box.y + 24, HAlignCenter | VAlignMiddle);
-		gmenu2x->s->clearClipRect();
-	}
+		// Darken background
+		gmenu2x->s->box(0, 0, gmenu2x->resX, gmenu2x->resY, 0,0,0, fadeAlpha);
 
-	// gmenu2x->s->box(ix + box.x, box.y, (box.w - ix), box.h, strtorgba("ffff00ff"));
-	gmenu2x->s->write(gmenu2x->font, text, ix + box.x + (box.w - ix) / 2, box.y + box.h / 2, HAlignCenter | VAlignMiddle, gmenu2x->skinConfColors[COLOR_FONT_ALT], gmenu2x->skinConfColors[COLOR_FONT_ALT_OUTLINE]);
+		fadeAlpha = intTransition(0, bgalpha, tickStart, 200);
+
+		// outer box
+		gmenu2x->s->box(box, gmenu2x->skinConfColors[COLOR_MESSAGE_BOX_BG]);
+
+		// draw inner rectangle
+		gmenu2x->s->rectangle(box.x + 2, box.y + 2, box.w - 4, box.h - 4, gmenu2x->skinConfColors[COLOR_MESSAGE_BOX_BORDER]);
+
+		// icon+text
+		if (gmenu2x->sc[icon] != NULL) {
+			gmenu2x->s->setClipRect({box.x + 8, box.y + 8, 32, 32});
+			gmenu2x->sc[icon]->blit(gmenu2x->s, box.x + 24, box.y + 24, HAlignCenter | VAlignMiddle);
+			gmenu2x->s->clearClipRect();
+		}
+
+		// gmenu2x->s->box(ix + box.x, box.y, (box.w - ix), box.h, strtorgba("ffff00ff"));
+		gmenu2x->s->write(gmenu2x->font, text, ix + box.x + (box.w - ix) / 2, box.y + box.h / 2, HAlignCenter | VAlignMiddle, gmenu2x->skinConfColors[COLOR_FONT_ALT], gmenu2x->skinConfColors[COLOR_FONT_ALT_OUTLINE]);
+
+		if (!this->autohide) {
+			// draw buttons rectangle
+			gmenu2x->s->box(box.x, box.y+box.h, box.w, gmenu2x->font->getHeight(), gmenu2x->skinConfColors[COLOR_MESSAGE_BOX_BG]);
+
+			int btnX = (gmenu2x->resX + box.w) / 2 - 6;
+			for (uint32_t i = 0; i < buttonText.size(); i++) {
+				if (!buttonText[i].empty()) {
+					buttonPosition[i].y = box.y+box.h+gmenu2x->font->getHalfHeight();
+					buttonPosition[i].w = btnX;
+
+					btnX = gmenu2x->drawButtonRight(gmenu2x->s, button[i], buttonText[i], btnX, buttonPosition[i].y);
+
+					buttonPosition[i].x = btnX;
+					buttonPosition[i].w = buttonPosition[i].x-btnX-6;
+				}
+			}
+		}
+
+		gmenu2x->s->flip();
+	} while (fadeAlpha < bgalpha);
 
 	if (this->autohide) {
-		gmenu2x->s->flip();
+		// gmenu2x->s->flip();
 		if (this->autohide > 0) SDL_Delay(this->autohide);
 		gmenu2x->powerManager->resetSuspendTimer(); // prevent immediate suspend
 		return -1;
 	}
 
-	//draw buttons rectangle
-	gmenu2x->s->box(box.x, box.y+box.h, box.w, gmenu2x->font->getHeight(), gmenu2x->skinConfColors[COLOR_MESSAGE_BOX_BG]);
-
-	int btnX = (gmenu2x->resX + box.w) / 2 - 6;
-	for (uint32_t i = 0; i < buttonText.size(); i++) {
-		if (buttonText[i] != "") {
-			buttonPosition[i].y = box.y+box.h+gmenu2x->font->getHalfHeight();
-			buttonPosition[i].w = btnX;
-
-			btnX = gmenu2x->drawButtonRight(gmenu2x->s, button[i], buttonText[i], btnX, buttonPosition[i].y);
-
-			buttonPosition[i].x = btnX;
-			buttonPosition[i].w = buttonPosition[i].x-btnX-6;
-		}
-	}
-	gmenu2x->s->flip();
-
 	while (result < 0) {
-		//touchscreen
+		// touchscreen
 		// if (gmenu2x->f200 && gmenu2x->ts.poll()) {
 		// 	for (uint32_t i = 0; i < buttonText.size(); i++) {
 		// 		if (buttonText[i] != "" && gmenu2x->ts.inRect(buttonPosition[i])) {
