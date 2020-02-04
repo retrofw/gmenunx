d3d627cd25d2fdb44a29b2c6e121719cf93e52be
pingflood <pingflood@gmail.com>
2020-02-04 18:35:24 -0300

    Remount home

--- src/hw/retrogame.h
+++ src/hw/retrogame.h
@@ -185,7 +185,7 @@ uint32_t hwCheck(unsigned int interval = 0, void *param = NULL) {
 class GMenuNX : public GMenu2X {
 private:
 	void hwInit() {
-		system("[ -d /home/retrofw ] && mount -o remount,rw,async,noatime,iocharset=utf8,shortname=win95 /home/retrofw");
+		system("[ -d /home/retrofw ] && mount -o remount,async /home/retrofw");
 
 		setenv("SDL_NOMOUSE", "1", 1);
 		memdev = open("/dev/mem", O_RDWR);
@@ -371,7 +371,7 @@ public:
 	}
 
 	string hwPreLinkLaunch() {
-		system("[ -d /home/retrofw ] && mount -o remount,rw,sync,noatime,iocharset=utf8,shortname=win95 /home/retrofw");
+		system("[ -d /home/retrofw ] && mount -o remount,sync /home/retrofw");
 		return "";
 	}
 };
