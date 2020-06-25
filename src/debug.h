62cb35bc7f86480ddb863438ffbcce819566a781
pingflood <pingflood@gmail.com>
2020-06-25 14:59:24 -0300

    Simplify debug macros

--- src/debug.h
+++ src/debug.h
@@ -7,61 +7,45 @@
 #define INFO_L 3
 #define DEBUG_L 4
 
+#define DEBUG_COLOR		"\e[1;34m"
+#define WARNING_COLOR	"\e[1;33m"
+#define ERROR_COLOR		"\e[1;31m"
+#define INFO_COLOR		"\e[1;32m"
+#define CLEAR_COLOR		"\e[00m\n"
+
 #ifndef LOG_LEVEL
 	#define LOG_LEVEL INFO_L
 #endif
 
-#ifndef DEBUG_START
-	#define DEBUG_START		"\e[1;34m"
-#endif
-#ifndef WARNING_START
-	#define WARNING_START	"\e[1;33m"
-#endif
-#ifndef ERROR_START
-	#define ERROR_START		"\e[1;31m"
-#endif
-#ifndef INFO_START
-	#define INFO_START		"\e[1;32m"
-#endif
-
-#define LOG_END "\e[00m\n"
-
-
 #define D(str, ...) \
-	fprintf(stdout, DEBUG_START "%s:%d %s: " str LOG_END, __FILE__, __LINE__, __func__,  ##__VA_ARGS__)
+	fprintf(stdout, ERROR_COLOR "%s:%d %s: " str CLEAR_COLOR, __FILE__, __LINE__, __func__,  ##__VA_ARGS__)
 
 #if (LOG_LEVEL >= DEBUG_L)
 	#define DEBUG(str, ...) \
-	fprintf(stdout, DEBUG_START str LOG_END, ##__VA_ARGS__)
+	fprintf(stdout, DEBUG_COLOR str CLEAR_COLOR, ##__VA_ARGS__)
+#else
+	#define DEBUG(...)
 #endif
 
 #if (LOG_LEVEL >= INFO_L)
 	#define INFO(str, ...) \
-	fprintf(stdout, INFO_START str LOG_END, ##__VA_ARGS__)
+	fprintf(stdout, INFO_COLOR str CLEAR_COLOR, ##__VA_ARGS__)
+#else
+	#define INFO(...)
 #endif
 
 #if (LOG_LEVEL >= WARNING_L)
 	#define WARNING(str, ...) \
-	fprintf(stderr, WARNING_START str LOG_END, ##__VA_ARGS__)
+	fprintf(stderr, WARNING_COLOR str CLEAR_COLOR, ##__VA_ARGS__)
+#else
+	#define WARNING(...)
 #endif
 
 #if (LOG_LEVEL >= ERROR_L)
 	#define ERROR(str, ...) \
-	fprintf(stderr, ERROR_START str LOG_END, ##__VA_ARGS__)
-#endif
-
-
-#ifndef DEBUG
-	#define DEBUG(...)
-#endif
-#ifndef WARNING
-	#define WARNING(...)
-#endif
-#ifndef ERROR
+	fprintf(stderr, ERROR_COLOR str CLEAR_COLOR, ##__VA_ARGS__)
+#else
 	#define ERROR(...)
-#endif
-#ifndef INFO
-	#define INFO(...)
 #endif
 
 #endif // DEBUG_H
