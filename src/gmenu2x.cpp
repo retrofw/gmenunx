7f877872efe724d6b1204cbbe60d75211df7c3c3
pingflood <pingflood@gmail.com>
2020-08-25 11:52:16 -0300

    tvOutPrev and tvOutPrev logic

--- src/gmenu2x.cpp
+++ src/gmenu2x.cpp
@@ -172,7 +172,7 @@ int16_t getUDCStatus(void) {
 	return UDC_ERROR;
 }
 
-int16_t tvOutPrev, tvOutConnected;
+int16_t tvOutPrev = false, tvOutConnected;
 bool getTVOutStatus() {
 	if (memdev > 0) return !(memregs[0x10300 >> 2] >> 25 & 0b1);
 	return false;
