a6ee853a179c2e60a0044fde8e888a9be82137a3
pingflood <pingflood@gmail.com>
2020-08-25 01:22:24 -0300

    Update manual search routine

--- src/linkapp.cpp
+++ src/linkapp.cpp
@@ -138,6 +138,7 @@ const string &LinkApp::searchManual() {
 	string linktitle = base_name(file);
 	pos = linktitle.rfind(".");
 	if (pos != string::npos) linktitle = linktitle.substr(0, pos);
+	linktitle += ".man.txt";
 
 	if (fileExists(linktitle))
 		manual = linktitle;
